5ee322a6965916575bff68b37a63b45f501ddbb1
sbelka-1703 <sbelka93@gmail.com>
2022-04-28 13:21:34 -0700

    reverted the changes

--- components/buttons/CopyButton.js
+++ components/buttons/CopyButton.js
@@ -7,10 +7,14 @@ function CopyButton(props) {
   const UncopiedButton = () => {
     return (
       <div className=" flex flex-col justify-center items-center">
+
         <button onClick={onCopyButtonClickHandler} >
+
           <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 hover:text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
           </svg>
+
+
         </button>
         <span className="text-[9px]">Press to copy</span>
       </div>
@@ -20,15 +24,22 @@ function CopyButton(props) {
   const CopiedButton = () => {
     return (
       <div className=" flex flex-col justify-center items-center">
+
         <button>
+
           <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 text-green-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
           </svg>
+
+
         </button>
         <span className="text-[9px]">Copied!</span>
       </div>
     )
   }
+
+
+
 
   const onCopyButtonClickHandler = () => {
     navigator.clipboard.writeText(props.currentLotAndExp)
@@ -45,6 +56,8 @@ function CopyButton(props) {
     }, 1000);
   }
 
+
+
   return (
     <>
       {
@@ -52,6 +65,7 @@ function CopyButton(props) {
           <UncopiedButton /> :
           <CopiedButton />
       }
+
     </>
   )
 }
