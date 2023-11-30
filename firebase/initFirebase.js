9f3bce20e02573ecea79b87d80a6889f7d4fe65a
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-11-30 07:52:17 -0700

    deploy

--- firebase/initFirebase.js
+++ firebase/initFirebase.js
@@ -5,7 +5,6 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 console.log(process.env);
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
