b27fc5b304edece49712678e0d6678c8917b5cf8
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-12-07 11:15:27 -0700

    update

--- firebase/initFirebase.js
+++ firebase/initFirebase.js
@@ -2,6 +2,7 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
+//For Demo version
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
